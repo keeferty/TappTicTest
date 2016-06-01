d26751527f3a8155218f6b3251d46183b5b721a7
Pawel Weglewski <pawel.weglewski@getresponse.com>
2016-06-01 11:36:08 +0200

    Empty Project Added to repo

---
+++ TappticTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TappticTest
+//
+//  Created by Pawel Weglewski on 01/06/16.
+//  Copyright © 2016 Pawel Weglewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
